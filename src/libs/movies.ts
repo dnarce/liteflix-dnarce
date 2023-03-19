a2b884c8836a0434e33d42b13cae3b9d99731da7
Diego Arce <dnarce@gmail.com>
2023-03-19 18:19:13 -0300

    Muevo la función `getRandomNumber` a utils para sacarla del archivo movies.ts

--- src/libs/movies.ts
+++ src/libs/movies.ts
@@ -1,6 +1,4 @@
-const getRandomNumber = (min: number, max: number): number => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { getRandomNumber } from './mathUtils';
 
 interface Movie {
   adult: boolean;

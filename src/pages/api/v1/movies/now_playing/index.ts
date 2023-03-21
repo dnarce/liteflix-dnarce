b565d933f412042dc954cb152f70b4c3d36feeb8
Diego Arce <dnarce@gmail.com>
2023-03-21 18:46:34 -0300

    agrego interface ErrorResponse y parser en la api

--- src/pages/api/v1/movies/now_playing/index.ts
+++ src/pages/api/v1/movies/now_playing/index.ts
@@ -1,15 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ErrorResponse } from '@/interfaces/error_response';
+import { LiteFlixMovie, MovieTMDB } from '@/interfaces/movies';
 import { getNowPlaying } from '@/services/movies';
 import type { NextApiRequest, NextApiResponse } from 'next';
-//TODO crear mi interfaz para la respuesta de mi API
-//TODO Crea un parser para pasar de interface de TMDB a mi API interface
+
+//TODO: Mover este parser
+const parseMovie = (movie: MovieTMDB): LiteFlixMovie => ({
+  adult: movie.adult,
+  backdropPath: movie.backdrop_path,
+  genreIds: movie.genre_ids,
+  id: movie.id,
+  originalLanguage: movie.original_language,
+  originalTitle: movie.original_title,
+  overview: movie.overview,
+  popularity: movie.popularity,
+  posterPath: movie.poster_path,
+  releaseDate: movie.release_date,
+  title: movie.title,
+  video: movie.video,
+  voteAverage: movie.vote_average,
+  voteCount: movie.vote_count,
+});
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<LiteFlixMovie | ErrorResponse>
 ) {
   try {
     const nowPlaying = await getNowPlaying();
-    res.status(200).json(nowPlaying);
+    res.status(200).json(parseMovie(nowPlaying));
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ error: error.message });

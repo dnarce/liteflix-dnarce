7962c42b059b59281dd2be851864446238385b44
Diego Arce <dnarce@gmail.com>
2023-03-23 09:03:49 -0300

    Agreo localmente las Fonts Bebas Neue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,26 @@ import { Hero } from '@/components/hero';
 import { Navbar } from '@/components/navbar';
 import { GetStaticProps } from 'next';
 
+import localFont from 'next/font/local';
+
+const bebasNeue = localFont({
+  src: [
+    {
+      path: '../../public/fonts/BebasNeueLight.ttf',
+      weight: '200',
+    },
+    {
+      path: '../../public/fonts/BebasNeueRegular.ttf',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/BebasNeueBold.ttf',
+      weight: '700',
+    },
+  ],
+  variable: '--font-bebasNeue',
+});
+
 export const getStaticProps: GetStaticProps = async (context) => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const response = await fetch(`${apiUrl}/api/v1/movies/now_playing`);
@@ -17,15 +37,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 export default function Home(props: GetStaticProps) {
   return (
-    <div className={`bg-orange-400 relative`}>
+    <div className={`bg-orange-400 relative ${bebasNeue.className}`}>
       <Navbar />
       <Hero />
-      {/* <section className='bg-blue-800 min-h-screen'>
-        <h2>Original de LITEFLIX</h2>
-        <h1>La casa de papel</h1>
-        <button>Reproducir</button>
-        <button>+ Mi Lista</button>
-      </section> */}
       <section>
         <h1>
           Ver: <button>Poulares</button>

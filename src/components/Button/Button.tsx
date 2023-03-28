d59baec84d844536b9f90db624fd926d08f32688
Diego Arce <dnarce@gmail.com>
2023-03-28 11:19:29 -0300

    Fix componente Button

--- src/components/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { default as ButtonPrimary } from './ButtonPrimary';
-import { default as ButtonSecondary } from './ButtonSecondary';
-import { default as ButtonLink } from './ButtonLink';
-import { default as ButtonInverse } from './ButtonInverse';
-
-export { ButtonPrimary, ButtonSecondary, ButtonLink, ButtonInverse };

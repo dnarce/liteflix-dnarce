3aadf1718e8e7de17c3902aa7c2c14e3dbe43d7b
Diego Arce <dnarce@gmail.com>
2023-03-28 20:54:07 -0300

    agrego framer motion y animo el Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,7 @@
 import { LiteFlixMovie } from '../interfaces/movies';
 import { ButtonPrimary, ButtonSecondary } from './Buttons';
 import { PlayIcon, PlusIcon } from './icons';
+import { motion } from 'framer-motion';
 
 interface HeroProps {
   movie: LiteFlixMovie;
@@ -10,11 +11,18 @@ export function Hero({ movie }: HeroProps) {
   const { originalTitle, backdropPath, isLiteFlixOriginal = true } = movie;
   return (
     <section
-      className='relative flex items-end md:items-center justify-center md:justify-start min-h-screen bg-no-repeat bg-center bg-cover pb-4 md:pb-0'
-      style={{
-        backgroundImage: `url('https://image.tmdb.org/t/p/original${backdropPath}')`,
-      }}
+      className='relative flex items-end md:items-center justify-center md:justify-start min-h-screen pb-4 md:pb-0 overflow-hidden'
+      // style={{
+      //   backgroundImage: `url('https://image.tmdb.org/t/p/original${backdropPath}')`,
+      // }}
     >
+      <motion.img
+        className='absolute bottom-0 left-0 w-screen h-screen bg-no-repeat bg-center object-cover'
+        src={`https://image.tmdb.org/t/p/original${backdropPath}`}
+        initial={{ scale: 1.5 }}
+        animate={{ scale: 1 }}
+        transition={{ type: 'tween', duration: 10 }}
+      ></motion.img>
       <div className='absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-dark-grey to-transparent lg:invisible'></div>
       <div className='text-center md:text-left md:pl-24 text-white z-10 md:absolute md:bottom-0 md:mb-40 lg:max-w-2/3-screen'>
         {isLiteFlixOriginal && (

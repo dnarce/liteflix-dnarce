2ed1789563cb43359289d8872329bf8dd5a4574b
Diego Arce <dnarce@gmail.com>
2023-03-29 13:15:08 -0300

    realizo cambios para no llamar a la api local

--- src/utils/moviesApi.ts
+++ src/utils/moviesApi.ts
@@ -1,13 +1,12 @@
+import { getNowPlaying, getPopularMovies } from '@/services/movies';
+import { parseMovie, parseMovies } from './moviesParser';
+
 export async function loadNowPlayingMovies() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const response = await fetch(`${apiUrl}/api/v1/movies/now_playing`);
-  const data = await response.json();
-  return data;
+  const nowPlaying = await getNowPlaying();
+  return parseMovie(nowPlaying);
 }
 
 export async function loadPopularMovies() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const response = await fetch(`${apiUrl}/api/v1/movies/popular`);
-  const data = await response.json();
-  return data;
+  const popularMovies = (await getPopularMovies()).slice(0, 4);
+  return parseMovies(popularMovies);
 }

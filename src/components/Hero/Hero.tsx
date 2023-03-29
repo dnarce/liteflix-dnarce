ac2f30cd228a4c0327bb40b7e04a64429dfd1aa3
Diego Arce <dnarce@gmail.com>
2023-03-29 10:36:57 -0300

    fix import

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,7 +12,7 @@ import {
   heroLiteflixOriginalClasses,
   heroOriginalTitleClasses,
   heroBodyContainerClasses,
-} from './Hero.Styles';
+} from './Hero.styles';
 
 interface HeroProps {
   movie: LiteFlixMovie;

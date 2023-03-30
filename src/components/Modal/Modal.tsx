e2445d07a0c1abeafdbbd7e7a810ad6858cc303a
Diego Arce <dnarce@gmail.com>
2023-03-29 22:29:38 -0300

    remuevo archivo classes Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,11 +2,6 @@ import React, { MouseEventHandler } from 'react';
 import classNames from 'classnames';
 import { CloseIcon } from '../icons';
 import { ButtonLink } from '../Buttons';
-import {
-  modalClasses,
-  contentClasses,
-  closeButtonContainerClasses,
-} from './modal.styles';
 
 interface ModalProps {
   isOpen: boolean;
@@ -15,25 +10,58 @@ interface ModalProps {
 }
 
 export function Modal({ isOpen, onClose, children }: ModalProps) {
-  const modalContainerClasses = classNames(modalClasses, {
-    'opacity-0 pointer-events-none': !isOpen,
-    'opacity-100': isOpen,
-  });
-
-  const modalContentClasses = classNames(contentClasses, {
-    'translate-y-full': !isOpen,
-    'translate-y-0': isOpen,
-  });
-
   return (
-    <div className={classNames(modalContainerClasses)}>
+    <div
+      className={classNames(
+        'fixed',
+        'inset-0',
+        'flex',
+        'items-center',
+        'justify-center',
+        'md:p-4',
+        'bg-black',
+        'bg-opacity-50',
+        'transition-opacity',
+        'z-50',
+        {
+          'opacity-0 pointer-events-none': !isOpen,
+          'opacity-100': isOpen,
+        }
+      )}
+    >
       <div
-        className={modalContentClasses}
+        className={classNames(
+          'w-full',
+          'h-full',
+          'md:max-h-[440px]',
+          'md:max-w-[730px]',
+          'md:items-center',
+          'flex',
+          'flex-col',
+          'justify-center',
+          'md:block',
+          'px-4',
+          'md:px-16',
+          'py-4',
+          'md:py-6',
+          'bg-dark-grey',
+          'transition-all',
+          'duration-300',
+          'transition-transform',
+          'transform',
+          'origin-bottom',
+          {
+            'translate-y-full': !isOpen,
+            'translate-y-0': isOpen,
+          }
+        )}
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
-        <div className={closeButtonContainerClasses}>
+        <div
+          className={classNames('md:flex', 'w-full', 'justify-end', 'hidden')}
+        >
           <ButtonLink icon={<CloseIcon />} onClick={onClose}></ButtonLink>
         </div>
         {children}
